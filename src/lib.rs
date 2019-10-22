a4eeeaa7a909dc579cb48908477bf218b535c50a
leo60228 <iakornfeld@gmail.com>
2019-10-22 14:03:43 +0000

    better heuristic

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,12 @@ pub struct GameState<'a> {
 #[derive(Debug)]
 pub struct Entry<'a> {
     pub typ: EntryType,
+    pub flags: u32,
     pub unknown: &'a [u8],
     pub name: String,
     pub raw_length: Option<u64>,
     pub magic: Option<u32>,
-    data: Vec<u8>,
+    pub data: Vec<u8>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -38,30 +39,39 @@ fn c_str(inp: &[u8]) -> IResult<&[u8], String> {
 
 fn entry_type(inp: &[u8]) -> IResult<&[u8], EntryType> {
     branch::alt((
-        combinator::value(
-            EntryType::File,
-            bytes::tag(&[0x20u8, 0x97u8, 0xb4u8, 0x81u8]),
-        ),
-        combinator::value(
-            EntryType::Directory,
-            bytes::tag(&[0x20u8, 0xb2u8, 0xfdu8, 0x41u8]),
-        ),
+        combinator::value(EntryType::File, bytes::tag(&[0xb1u8, 0xcau8])),
+        combinator::value(EntryType::Directory, bytes::tag(&[0x01u8, 0xa4u8])),
     ))(inp)
 }
 
+fn first_match<'a>(strings: &'a [&[u8]]) -> impl Fn(&[u8]) -> IResult<&[u8], &[u8]> + 'a {
+    move |inp| {
+        let possibilities = strings.iter().copied().map(|s| bytes::take_until(s)(inp));
+        possibilities
+            .min_by_key(|r| {
+                if let Ok((_, o)) = r {
+                    o.len() as isize
+                } else {
+                    -1
+                }
+            })
+            .unwrap()
+    }
+}
+
 pub fn entry(inp: &[u8]) -> IResult<&[u8], Entry> {
-    let (r, (typ, unknown, name)) =
-        sequence::tuple((entry_type, bytes::take(28usize), c_str))(inp)?;
-    let (r, (raw_length, magic, data, _)) = sequence::tuple((
+    let (r, (typ, flags, unknown, name)) =
+        sequence::tuple((entry_type, number::le_u32, bytes::take(28usize), c_str))(inp)?;
+    let (r, (raw_length, magic, data)) = sequence::tuple((
         combinator::cond(typ == EntryType::File, number::le_u64),
         combinator::cond(typ == EntryType::File, number::le_u32),
-        branch::alt((bytes::take_until(b"\xb1\xca" as &[u8]), combinator::rest)),
-        combinator::opt(bytes::tag(b"\xb1\xca" as &[u8])),
+        branch::alt((first_match(&[&[0x01, 0xa4], &[0xb1, 0xca]]), combinator::rest)),
     ))(r)?;
     Ok((
         r,
         Entry {
             typ,
+            flags,
             unknown,
             name,
             raw_length,
@@ -72,7 +82,7 @@ pub fn entry(inp: &[u8]) -> IResult<&[u8], Entry> {
 }
 
 pub fn gamestate(inp: &[u8]) -> IResult<&[u8], GameState> {
-    let (r, (header, entries)) = sequence::tuple((bytes::take(10usize), multi::many0(entry)))(inp)?;
+    let (r, (header, entries)) = sequence::tuple((bytes::take(8usize), multi::many0(entry)))(inp)?;
     Ok((r, GameState { header, entries }))
 }
 
